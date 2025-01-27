fdc6af3043de94171c653933368262ba0a331578
sky17892 <46701383+sky17892@users.noreply.github.com>
2025-01-27 17:28:47 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,11 @@ const IndexPage = ({ externalUrl }: Props) => {
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div style={{  position: 'relative',  height: 0,  paddingBottom: '100%', overflow: 'scroll' }}>
+      <div style={{  position: 'relative',  height: 0,  paddingBottom: '800px', overflow: 'hidden' }}>
       <iframe
         src={externalUrl}
         width="100%"
-        height="100%"
+        height="800px"
         style={{ position: 'absolute', top: 0, left: 0, border: 'none' }}
         title="nextjsloadingsite"
       />
