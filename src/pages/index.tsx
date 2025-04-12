a8f26feacf1d62c3706433aeeebb30f199ad3f34
sky17892 <46701383+sky17892@users.noreply.github.com>
2025-04-12 19:26:38 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 interface Props {
-  externalUrl1234567: string;
+  externalUrl777: string;
 }
 
-const IndexPage = ({ externalUrl1234567 }: Props) => {
+const IndexPage = ({ externalUrl777 }: Props) => {
   return (
     <>
     <Head>
@@ -13,7 +13,7 @@ const IndexPage = ({ externalUrl1234567 }: Props) => {
       </Head>
       <div style={{  position: 'relative',  height: 0,  paddingBottom: '865px', overflow: 'hidden' }}>
       <iframe
-        src={externalUrl1234567}
+        src={externalUrl777}
         width="100%"
         height="865px"
         style={{ position: 'absolute', top: 0, left: 0, border: 'none' }}
@@ -26,11 +26,11 @@ const IndexPage = ({ externalUrl1234567 }: Props) => {
 
 // 서버 측에서 외부 URL을 전달하는 코드
 export const getServerSideProps: GetServerSideProps = async () => {
-  const externalUrl1234567 = "https://molln.in/";
+  const externalUrl777 = "https://molln.in/";
 
   return {
     props: {
-      externalUrl1234567, // props로 외부 URL 전달
+      externalUrl777, // props로 외부 URL 전달
     },
   };
 };
